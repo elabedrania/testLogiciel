84d41608651b266b9ba33c17279e6b7d2d952ae5
elabedrania <raniiaelabed@gmail.com>
2023-01-11 13:59:23 +0100

    first commit

---
+++ Classcalculbasic.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculBasic
+{
+    public class Classcalculbasic
+    {
+        public bool Add(int x,int y )
+        {
+            return (( x + y)<0);
+            
+
+        }
+        public bool Substract(int x, int y)
+        {
+            return x > y;
+
+        }
+        public bool Multiple(int x, int y)
+        {
+            int r = x * y;
+            return r>0 ;
+
+        }
+        public bool Div(int x, int y)
+        {
+            return x / y>0;
+
+        }
+
+
+
+    }
+}
---
+++ Logger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testlogiciel
+{
+  public class Logger
+    {
+       
+
+        public bool IsValidLogFile(string FileName)
+        {
+            if (FileName.ToUpper().EndsWith("SDF"))
+                return true;
+            return false;
+            
+        }
+    }
+}
